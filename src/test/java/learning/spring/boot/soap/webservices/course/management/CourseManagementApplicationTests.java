882619430123b11a8c11bf4b06305814031fffba
Romit Modi <modiromit7@gmail.com>
2020-12-18 14:44:33 +0530

    inital commit

---
+++ src/test/java/learning/spring/boot/soap/webservices/course/management/CourseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.spring.boot.soap.webservices.course.management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
